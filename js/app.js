880b8c41599c54a6fc46a24bd331a951343c5ee6
Ajit <47279189+ajits01@users.noreply.github.com>
2022-04-09 23:56:50 +0530

    🎯 Initial commit

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const OWAK = "dad6388d479dda69200617271e376995";
+
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+
+		var map = $(".map");
+		var latitude = map.data("latitude");
+		var longitude = map.data("longitude");
+		if( map.length ){
+			
+			map.gmap3({
+				map:{
+					options:{
+						center: [latitude,longitude],
+						zoom: 15,
+						scrollwheel: false
+					}
+				},
+				marker:{
+					latLng: [latitude,longitude],
+				}
+			});
+			
+		}
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(() => {
+    console.log("Registered");
+  });
+}
